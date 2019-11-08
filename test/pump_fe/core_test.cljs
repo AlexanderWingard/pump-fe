ebb192bf7f617bc4a75644bfa9404fb36d2a969d
Alexander Wingård <alexander.wingard@gmail.com>
2019-11-08 14:10:41 +0100

    Initial commit from figwheel-main template

---
+++ test/pump_fe/core_test.cljs
@@ -0,0 +1,10 @@
+(ns pump-fe.core-test
+    (:require
+     [cljs.test :refer-macros [deftest is testing]]
+     [pump-fe.core :refer [multiply]]))
+
+(deftest multiply-test
+  (is (= (* 1 2) (multiply 1 2))))
+
+(deftest multiply-test-2
+  (is (= (* 75 10) (multiply 10 75))))
